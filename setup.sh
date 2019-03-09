d958acc64ede2f662d34d1c594dff2808478577e
Syed Ali <pepsiplease69@yahoo.com>
2019-03-09 13:21:39 -0800

    Setup File

---
+++ setup.sh
@@ -0,0 +1,19 @@
+
+
+
+
+
+sudo apt-get install git
+git clone https://github.com/pepsiplease69/sunrise.git
+cd sunrise/
+chmod a+x *.pl 
+sudo cpan App::cpanminus
+sudo cpanm DateTime
+sudo cpanm DateTime::Duration
+sudo cpanm DateTime::Event::Sunrise
+sudo cpanm IO::Async::Timer::Absolute
+sudo cpanm IO::Async::Loop
+sudo cpanm LWP::Simple
+
+
+
