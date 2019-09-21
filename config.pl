b497a4085b516fd8c81fcb82662e1d47a443d80f
Syed Ali <pepsiplease69@yahoo.com>
2019-09-21 16:40:52 -0700

    added functions for playing sounds

--- config.pl
+++ config.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl -w
+
 use strict;
 use warnings;
 
@@ -6,3 +7,27 @@ our ( $mylong );
 our ( $mylat );
 $mylong     = 39.826186;
 $mylat      = 21.422459;
+
+
+sub play_azan
+{
+    system ( "/usr/bin/mpg321 /home/pi/sounds/adhan.mp3" );
+}
+
+sub announce_sunrise
+{
+    system ( "/usr/bin/mpg321 /home/pi/sounds/zen.mp3" );
+    system ( "/usr/bin/mpg321 /home/pi/sounds/sunrise.mp3" );
+}
+
+sub announce_sunset
+{
+    system ( "/usr/bin/mpg321 /home/pi/sounds/zen.mp3" );
+    system ( "/usr/bin/mpg321 /home/pi/sounds/sunset.mp3" );
+}
+
+sub announce_twilight
+{
+    system ( "/usr/bin/mpg321 /home/pi/sounds/zen.mp3" );
+    system ( "/usr/bin/mpg321 /home/pi/sounds/twilight.mp3" );
+}
