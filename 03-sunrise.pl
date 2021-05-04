bbb25e02917c4afa2b3b7645eb34298dfc766145
Syed Ali <pepsiplease69@yahoo.com>
2021-05-04 08:01:55 -0700

    renaming all files

---
+++ 03-sunrise.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+
+use DateTime;
+use DateTime::Duration;
+use DateTime::Event::Sunrise;
+use strict;
+
+use IO::Async::Timer::Absolute;
+use IO::Async::Loop;
+
+use LWP::Simple;
+
+  my $today_dt = DateTime->today( time_zone => "local" );
+
+our ( $mylong );
+our ( $mylat );
+require ( "/home/pi/sunrise/config.pl" );
+
+
+
+  my $sun_Martinez  = DateTime::Event::Sunrise->new (
+                        longitude => $mylong,
+                        latitude  => $mylat);
+
+
+
+##################################################
+
+
+# generating DateTime objects from a DateTime::Event::Sunrise object
+my $sunrise_dt = $sun_Martinez->sunrise_datetime($today_dt);
+
+
+
+my $tmp1_dt = $sunrise_dt->clone;
+$tmp1_dt->set_time_zone( 'local');
+
+print  "Date: " . $today_dt->datetime . "\n" .
+       "Sunrise: " . $sunrise_dt->hms . "\n" .
+       "Alarm: " . $tmp1_dt->hms . "\n\n\n";
+
+
+
+my $loop1  = IO::Async::Loop->new;
+my $timer1 = IO::Async::Timer::Absolute->new(
+time      => $tmp1_dt->epoch(),
+on_expire => sub {
+                    announce_sunrise (  );
+                    $loop1->stop;
+                 },
+        );
+
+
+
+
+$loop1->add ( $timer1 );
+$loop1->run;
+
+
+
+
+
+
+
+
+
---
+++ 07-moonrise.pl
@@ -0,0 +1,91 @@
+#!/usr/bin/perl
+
+
+
+use DateTime;
+use IO::Async::Timer::Absolute;
+use IO::Async::Loop;
+use LWP::Simple;
+use Astro::Coord::ECI;
+use Astro::Coord::ECI::Moon;
+use Astro::Coord::ECI::Utils qw{deg2rad};
+use strict;
+ 
+
+
+
+our ( $mylong );
+our ( $mylat );
+our ( $myalt );
+require ( "/home/pi/sunrise/config.pl" );
+
+
+
+
+my $lat = deg2rad ($mylat);    # Radians
+my $long = deg2rad ($mylong);  # Radians
+my $alt = $myalt / 1000;        # Kilometers
+my $moon = Astro::Coord::ECI::Moon -> new ();
+my $sta = Astro::Coord::ECI ->
+                        universal (time ()) ->
+                        geodetic ($lat, $long, $alt);
+my ($moon_time, $rise) = $sta->next_elevation ($moon);
+print "\tMoon @{[$rise ? 'rise' : 'set']} is ",
+    scalar localtime $moon_time, "\t";
+
+
+
+if ( $rise )
+{
+    print "rise next \t";
+}
+else
+{
+    print "set  next \n";
+    exit;
+}
+
+
+# generating DateTime objects from a DateTime::Event::Sunrise object
+my $moonset_dt = DateTime->from_epoch ( epoch => $moon_time );
+$moonset_dt->set_time_zone( 'local');
+
+if ( ( $moonset_dt->hour < 16 )  ||
+     ( $moonset_dt->hour > 21 ) )
+{
+    #  if moonset is occurring before 4pm or after 10pm (technically 9:59) then don't set alarm
+    #  it goes to the next calendar day and starts reporting the prior day's moonset time
+    print "moonrise is out of bounds (16,21) exiting\n";
+    exit;
+}
+
+
+
+#-------------------------------------------------------------
+
+
+
+
+
+
+##################################################
+
+
+
+print  "Date: " .  $moonset_dt->mdy . " " .
+       "Alarm: " . $moonset_dt->hms . "\n" ;
+
+
+my $loop2  = IO::Async::Loop->new;
+my $timer2 = IO::Async::Timer::Absolute->new(
+		time      => $moonset_dt->epoch(),
+		on_expire => sub {
+                    announce_moonise ( );
+					$loop2->stop;
+				 },
+		);
+
+$loop2->add ( $timer2 );
+$loop2->run;
+
+
